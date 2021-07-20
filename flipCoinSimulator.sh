123a4d1ad617ec7423873bb95d5ffe071e9206cc
ashwith31 <ashwitash@gmail.com>
2021-07-20 19:40:34 +0530

    modified:Records of heads and tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,17 @@
 #! /bin/bash -x
 
+counth=0
+countt=0
+for((i=0;i<100;i++))
+do
 flip=$(( RANDOM%2 ))
 if [ $flip -eq 0 ]
 then
-echo "Heads is Winner"
+((counth++))
 else
-echo "Tails is winner"
+((countt++))
 fi
+done
+echo "---Out of 100 flips---"
+echo "Number of times heads won is $counth "
+echo "Number of times tails won is $countt "
